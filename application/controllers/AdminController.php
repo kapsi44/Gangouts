1a31c43ed51b658cd448e28ffc8eeedb85587472
aspire864 <aspire864@aspire864.Aspiresys.com>
2015-11-12 11:23:21 +0530

    Nov-12

--- application/controllers/AdminController.php
+++ application/controllers/AdminController.php
@@ -4,28 +4,12 @@
  *
  * PHP version 5
  *
- * @category  PHP
- * @package   PHP_CodeSniffer
- * @author    Kapil Sharma <kapsi44@gmail.com>
- * @copyright 1990-2015 Aspire Sys Ltd (ABN 77 084 670 600)
- * @license   https://github.com/squizlabs/PHP_CodeSniffer/blob/master/licence.txt BSD Licence
+ * 
+ * @author    Kapil Sharma <kapil.sharma@aspiresys.com>
  * @link      http://www.kapso.com/index.php
  */
 
 require_once 'application/config/config.php'; 
-
-/**
- * Parses and verifies the doc comments for files.
- *
- * PHP version 5
- *
- * @category  PHP
- * @package   PHP_CodeSniffer
- * @author    Kapil Sharma <kapsi44@gmail.com>
- * @copyright 2006-2014 Squiz Pty Ltd (ABN 77 084 670 600)
- * @license   https://github.com/squizlabs/PHP_CodeSniffer/blob/master/licence.txt BSD Licence
- * @link      http://www.kapso.com/index.php
- */
 
 class AdminController extends BaseController
 {
--- application/controllers/UserController.php
+++ application/controllers/UserController.php
@@ -5,11 +5,8 @@ require_once 'application/config/config.php';
  *
  * PHP version 5
  *
- * @category  PHP
- * @package   PHP_CodeSniffer
- * @author    Kapil Sharma <kapsi44@gmail.com>
- * @copyright 2006-2014 Squiz Pty Ltd (ABN 77 084 670 600)
- * @license   https://github.com/squizlabs/PHP_CodeSniffer/blob/master/licence.txt BSD Licence
+ * 
+ * @author    Kapil Sharma <kapil.sharma@aspiresys.com>
  * @link      http://www.kapso.com/index.php
  */
 class UserController extends BaseController 
